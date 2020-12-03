e28d9aed018d90fb55869785d783860400d137f3
jayabal <aljayabala@gmail.com>
2020-12-03 18:55:48 +0530

    Updates

---
+++ precache-manifest.13afc97f1f0135b65d4569e2fa32e38d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b7f45996453768ed3b5085d1f6af0b0b",
+    "url": "/covid_tracker/index.html"
+  },
+  {
+    "revision": "f93768212280eac4dc1b",
+    "url": "/covid_tracker/static/css/main.31b4e098.chunk.css"
+  },
+  {
+    "revision": "12c26581e4566892d094",
+    "url": "/covid_tracker/static/js/2.e771c284.chunk.js"
+  },
+  {
+    "revision": "1ef8d540b3072f991ff256dfc61c7bcd",
+    "url": "/covid_tracker/static/js/2.e771c284.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f93768212280eac4dc1b",
+    "url": "/covid_tracker/static/js/main.6e048d65.chunk.js"
+  },
+  {
+    "revision": "4c5145b2abee34e7044b",
+    "url": "/covid_tracker/static/js/runtime-main.fb184295.js"
+  },
+  {
+    "revision": "01c1107fc97ef44c833e17d9c01e9c20",
+    "url": "/covid_tracker/static/media/covid19-analysis.01c1107f.png"
+  }
+]);
